9100bed766cac7e8e369e74f9414a6725e576018
btabisz <bartosz.tabisz@gmail.com>
2022-11-23 10:19:53 +0100

    code update

--- main.py
+++ main.py
@@ -3,21 +3,19 @@ p = [3, 10, -6, 0, 8, 122, 14, -30, 12, 4, 200]
 s = sorted(p)
 print(s)
 
-szukana =int(input("podaj liczbę"))
+search_number = int(input("Podaj liczbę:"))
 
-lewy = 0
-prawy = len(s) - 1
+left = 0
+right = len(s) - 1
 
-
-while lewy <= prawy:
-    srodkowy = (lewy + prawy) // 2
-    if s[srodkowy] == szukana:
-        print(f'Szukana liczba ({szukana}) jest na liście! ')
+while left <= right:
+    mid = (left + right) // 2
+    if s[mid] == search_number:
+        print(f'Szukana liczba ({search_number}) jest na liście! ')
         break
-    elif s[srodkowy] < szukana:
-        lewy = srodkowy + 1
+    elif s[mid] < search_number:
+        left = mid + 1
     else:
-        prawy = srodkowy - 1
+        right = mid - 1
 else:
-    print(f'Liczby {szukana} nie ma na liście!')
-
+    print(f'Liczby {search_number} nie ma na liście!')
